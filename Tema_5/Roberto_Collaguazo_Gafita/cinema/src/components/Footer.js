c3e8fcd37217f4a4e9699cc784e0a212fa494de6
Roberto Collaguazo <150202269+Racollaguazo95@users.noreply.github.com>
2023-12-18 17:42:28 -0500

    Delete Tema_5/Roberto_Collaguazo_Gafita/cinema directory

--- Tema_5/Roberto_Collaguazo_Gafita/cinema/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Footer = () => {
-  return (
-    <footer className='header-footer'>
-        <p className='header-footer-references'>© 2023 UNIR CINEMA.</p>
-    </footer>
-  )
-}
